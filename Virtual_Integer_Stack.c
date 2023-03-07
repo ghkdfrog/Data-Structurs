bf04e6e97498e2927256ec3c077f95b265ba3e7b
unknown <ghkdfrog@gmail.com>
2023-03-07 23:06:22 +0900

    complete

--- Virtual_Integer_Stack.c
+++ Virtual_Integer_Stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX 10
+#define MAX 4
 
 typedef struct 
 {
@@ -13,16 +13,23 @@ void queueInit(Queue* q)
 {	q->front = -1;
 	q->rear = -1;
 }
-int queue_full(Queue* q)
+int queue_empty(Queue* q)
 {
-	if (q->rear == MAX - 1)
+	if (q->front == q->rear)
 	{
-		printf("queue full\n");
 		return 1;
 	}
 	return 0;
 }
-void enqueu(Queue* q, int data)
+int queue_full(Queue* q)
+{
+	if (q->rear == MAX - 1)
+	{
+		return 1;
+	}
+	return 0;
+}
+void enqueue(Queue* q, int data)
 {
 	q->rear++;
 	q->data[q->rear] = data;
@@ -34,7 +41,11 @@ int dequeue(Queue* q)
 }
 void printQueue(Queue* q)
 {
-	printf("front: %d, rear: %d\n", q->front, q->rear);
+	for (int i = q->front + 1; i <= q->rear; i++)
+	{
+		printf("[ %d ] ", q->data[i]);
+	}
+	printf("\n");
 }
 typedef struct
 {
@@ -61,11 +72,63 @@ void push(Stack* s, int data)
 	{
 		return;
 	}
-
+	enqueue(&(s->mainQueue), data);
+}
+int stack_empty(Stack* s)
+{
+	if (queue_empty(&(s->mainQueue)))
+	{
+		printf("stack empty\n");
+		return 1;
+	}
+	return 0;
+}
+int pop(Stack* s)
+{
+	if (stack_empty(s))
+	{
+		return -1;
+	}
+	int result;
+	while ((s->mainQueue).front + 1 < (s->mainQueue).rear)
+	{
+		enqueue(&(s->subQueue), dequeue(&(s->mainQueue)));
+	}
+	result = dequeue(&(s->mainQueue));
+	queueInit(&(s->mainQueue));
+	int res2;
+	while (!queue_empty(&(s->subQueue)))
+	{	
+		enqueue(&(s->mainQueue), dequeue(&(s->subQueue)));
+	}
+	queueInit(&(s->subQueue));
+	return result;
+}
+void printStack(Stack* s)
+{
+	printQueue(&(s->mainQueue));
 }
 int main()
 {
 	Stack s;
 	stackInit(&s);
+	pop(&s);
+	push(&s, 10);
+	push(&s, 20);
+	push(&s, 30);
+	push(&s, 40);
+	push(&s, 50);
+	printStack(&s);
+	printf("pop: %d\n", pop(&s));
+	printStack(&s);
+	printf("pop: %d\n", pop(&s));
+	printStack(&s);
+	push(&s, 50);
+	printStack(&s);
+	printf("pop: %d\n", pop(&s));
+	printStack(&s);
+	pop(&s);
+	pop(&s);
+	pop(&s);
 	return 0;
 }
