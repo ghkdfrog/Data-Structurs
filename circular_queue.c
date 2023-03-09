d8c77f6f2f6440050ae9615e7191b57b195fef7b
unknown <ghkdfrog@gmail.com>
2023-03-09 13:47:00 +0900

    fixed

--- circular_queue.c
+++ circular_queue.c
@@ -1,48 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define MAX 5
-int queue[MAX];
-int front = 0;
-int rear = 0;
+#include <stdbool.h>
 
-void insert(int item)
+#define MAX 6
+
+typedef struct
 {
-	if ((rear + 1) % MAX == front)
-	{
-		printf("queue full\n");
-		return ;
-	}
-	rear++;
-	queue[rear % MAX] = item;
+	int front;
+	int rear;
+	int data[MAX];
+}Queue;
 
-	return;
+Queue queue;
+
+void queue_init()
+{
+	queue.front = 0;
+	queue.rear = 0;
 }
 
-int delete()
+void enqueue(int item)
 {
-	if (front == rear)
+	queue.data[(queue.rear++)%MAX] = item;
+}
+
+int dequeue()
+{
+	return queue.data[(queue.front++) & MAX];
+}
+
+int queue_full()
+{
+	return (queue.rear + 1) % MAX == queue.front;
+}
+
+int queue_empty()
+{
+	return queue.rear == queue.front;
+}
+
+void print_queue()
+{
+	printf(" queue : ");
+	for (int i = queue.front; i < queue.rear; i++)
 	{
-		printf("queue empty\n");
-		return 0;
+		printf("[%d] ", queue.data[i%MAX]);
 	}
-	front++;
-	return queue[front % MAX];
+	printf("\n");
 }
 
 int main()
 {
-	int delete_num;
-	delete();
-	insert(1);
-	printf("delete: %d\n", delete());
-	for (int i = 1; i < 6; i++)
-	{
-		insert(i);
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		delete();
+	queue_init();
+	printf("enqueue : 1\n");
+	printf("dequeue : 2\n");
+	printf("print queue : p\n");
+	printf("quit program : q\n");
+
+	bool bol = true;
+
+	char key;
+	int item;
+	while (bol) {
+		printf("put key : ");
+		scanf(" %c", &key);
+
+		switch (key) {
+		case '1':
+			if (queue_full())
+			{
+				printf("	queue full\n");
+				break;
+			}
+			while (1)
+			{
+				printf("	item to enqueue : ");
+				scanf(" %d", &item);
+				if (item < 0)
+				{
+					printf("	item is bigger than 0\n");
+					continue;
+				}
+				enqueue(item);
+				break;
+			}
+			break;
+		case '2':
+			if (queue_empty())
+			{
+				printf("	queue empty\n");
+				break;
+			}
+			printf("	dequeue data: %d\n", dequeue());
+			break;
+		case 'q':
+			bol = false;
+			break;
+		case 'p':
+			if (queue_empty())
+			{
+				printf("	queue empty\n");
+				break;
+			}
+			print_queue();
+			break;
+		default:
+			printf("	wrong key\n");
+			break;
+		}
 	}
 	return 0;
 }
