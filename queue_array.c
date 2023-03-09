149b76104263c58d609ae07c544092d6ad243a73
ghkdfrog <125863672+ghkdfrog@users.noreply.github.com>
2023-03-09 20:07:53 +0900

    Add files via upload

--- queue_array.c
+++ queue_array.c
@@ -20,30 +20,34 @@ void queue_init() //queue initialize
 	queue.rear = -1;
 }
 
-int queue_full()
+int queue_full() //check queue full
 {
-	return (queue.rear == MAX - 1);
+	//if rear == MAX -1, return 1, else return 0.
+	return (queue.rear == MAX - 1); 
 }
 
-int queue_empty()
+int queue_empty() //check queue empty
 {
-	return (queue.front == queue.rear);
+	//if front == rear, return 1, else return 0.
+	return (queue.front == queue.rear); 
 }
 
 void enqueue(int item)
 {
-	queue.data[++queue.rear] = item;
+	// ++rear and insert the item in queue[rear].
+	queue.data[++queue.rear] = item; 
 }
 
 int dequeue()
 {
-	return queue.data[++queue.front];
+	// ++front and return the item in queue[front].
+	return queue.data[++queue.front]; 
 }
 
-void queue_print()
+void print_queue()
 {
 	printf(" queue : ");
-	for (int i = queue.front + 1; i <= queue.rear; i++)
+	for (int i = queue.front + 1; i <= queue.rear; i++) 
 	{
 		printf("[%d] ", queue.data[i]);
 	}
@@ -53,14 +57,17 @@ void queue_print()
 int main()
 {
 	queue_init();
+	// print menu.
 	printf("enqueue : 1\n");
 	printf("dequeue : 2\n");
 	printf("print queue : p\n");
 	printf("quit program : q\n");
 
+	// bool : stop loop.
 	bool bol = true;
-
+	// key : select menu.
 	char key;
+	// item :insert in queue.
 	int item;
 	while (bol) {
 		printf("put key : ");
@@ -68,18 +75,19 @@ int main()
 
 		switch (key) {
 		case '1':
-			if (queue_full())
+			if (queue_full()) // check queue full.
 			{
 				printf("	queue full\n");
 				break;
 			}
 			while (1)
 			{
+				while (getchar() != '\n');
 				printf("	item to enqueue : ");
 				scanf(" %d", &item);
-				if (item < 0)
+				if (item < 0) // check item is bigger than 0 and not number.
 				{
-					printf("	item is bigger than 0\n");
+					printf("	item is negetive or not number\n");
 					continue;
 				}
 				enqueue(item);
@@ -87,7 +95,7 @@ int main()
 			}
 			break;
 		case '2':
-			if (queue_empty())
+			if (queue_empty()) // check queue empty
 			{
 				printf("	queue empty\n");
 				break;
@@ -95,17 +103,19 @@ int main()
 			printf("	dequeue data: %d\n", dequeue());
 			break;
 		case 'q':
+			// stop loop
 			bol = false;
 			break;
 		case 'p':
-			if (queue_empty())
+			if (queue_empty()) // check queue empty.
 			{
 				printf("	queue empty\n");
 				break;
 			}
-			queue_print();
+			print_queue();
 			break;
 		default:
+			// check key is suitable.
 			printf("	wrong key\n");
 			break;
 		}
--- stack_array.c
+++ stack_array.c
@@ -12,28 +12,32 @@ typedef struct
 
 Stack stack;
 
-void stack_init()
+void stack_init() //stack initialize
 {
 	stack.top = -1;
 }
 
-int stack_empty()
+int stack_empty() //check stack empty
 {
+	//if top == -1, return 1, else return 0.
 	return (stack.top == -1);
 }
 
-int stack_full()
+int stack_full() //check stack full
 {
+	//if tio == MAX-1, return 1, else return 0.
 	return (stack.top == MAX - 1);
 }
 
 void push(int item)
 {
+	//++top and insert the item in stack[top].
 	stack.data[++stack.top] = item;
 }
 
 int pop()
 {
+	// return stack[top] and top--.
 	return stack.data[stack.top--];
 }
 
@@ -50,22 +54,24 @@ void stack_print()
 int main()
 {
 	stack_init();
+	//print menu.
 	printf("push : 1\n");
 	printf("pop : 2\n");
 	printf("print stack : p\n");
 	printf("quit program : q\n");
 	
+	// bool : stop loop
 	bool bol = true;
-
+	// key : select menu
 	char key;
+	// item : insert in stack
 	int item;
 	while (bol) {
-		
 		printf("put key : ");
 		scanf(" %c", &key);
 		switch (key) {
 		case '1':
-			if (stack_full())
+			if (stack_full()) // check stack full
 			{
 				printf("stack full\n");
 				break;
@@ -75,7 +81,7 @@ int main()
 				while (getchar() != '\n');
 				printf("	item to push : ");
 				scanf(" %d", &item);
-				if (item < 0)
+				if (item < 0) // check item is bigger than 0 and not number.
 				{
 					printf("	item is negetive or not number\n");
 					continue;
@@ -85,7 +91,7 @@ int main()
 			}
 			break;
 		case '2':
-			if (stack_empty())
+			if (stack_empty()) //check stack empty
 			{
 				printf("	stack empty\n");
 				break;
@@ -93,10 +99,11 @@ int main()
 			printf("	pop data: %d\n", pop());
 			break;
 		case 'q':
+			// stop loop
 			bol = false;
 			break;
 		case 'p':
-			if (stack_empty())
+			if (stack_empty()) // check stack empty
 			{
 				printf("	stack empty\n");
 				break;
@@ -104,6 +111,7 @@ int main()
 			stack_print();
 			break;
 		default:
+			// check key is suitable
 			printf("	wrong key\n");
 			break;
 		}
